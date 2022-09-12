ff8097bab7b4516e9f3e2e294be6a39183e4ef8f
Luca <91071096+LucaAvila123@users.noreply.github.com>
2022-09-12 14:39:57 -0300

    Add files via upload

---
+++ Aluno.java
@@ -0,0 +1,20 @@
+public class Aluno {
+    private String nome;
+    private String matricula;
+
+    public void setName(String nome){
+        this.nome = nome;
+    }
+
+    public String getName(){
+        return this.nome;
+    }
+
+    public void setMatricula(String matricula){
+        this.matricula = matricula;
+    }
+
+    public String getMatricula(){
+        return this.matricula;
+    }
+}
---
+++ Olha.java
@@ -0,0 +1,17 @@
+public class Olha{
+    
+    public static void main(String[] args){
+        //constructor do aluno
+        Aluno luca = new Aluno();
+    
+        luca.setName("Luca");
+        luca.setMatricula("2021100331");
+
+        System.out.println(luca.getName());
+        System.out.println(luca.getMatricula());
+
+        System.out.println("Maquina virtual");
+    }
+    
+
+}
